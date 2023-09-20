0ffcbc4357e98b83644cfb804fe8d2e40340a4a6
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-09-20 16:44:40 -0500

    Style up the login page

--- loginPage.php
+++ loginPage.php
@@ -6,16 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="RegisterLogin.css">
 </head>
 
 <body style="background-color: #508bfc;">
     <div class="m-5">
-        <a class="btn btn-lg btn-danger fs-5" href="index.html">Back to HomePage</a>
+        <a class="btn btn-lg fs-5" href="index.html" id="return-to-homepage">Back to HomePage</a>
     </div>
     <section class="vh-100">
         <div class="container m-5">
-            <div class="row d-flex">
+            <div class="row d-flex justify-content-center">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                     <div class="card shadow-2-strong" style="border-radius: 1rem;">
                         <div class="card-body p-5 text-center">
@@ -33,7 +33,7 @@
                                     <label class="form-label" for="typePassword">Password</label>
                                 </div>
 
-                                <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                                <button class="btn btn-lg" type="submit" id="login-btn">Login</button>
 
                             </form>
                         </div>
