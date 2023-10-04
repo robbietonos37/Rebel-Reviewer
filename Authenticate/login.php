840bd410f788918c77f155e6052d75c47a0726a7
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-10-04 14:36:19 -0500

    Add login to authenticate folder and write code to get userid

---
+++ Authenticate/login.php
@@ -0,0 +1,35 @@
+<?php
+
+echo '<script>';
+echo 'console.log(' . json_encode($_SERVER) . ')';
+echo '</script>';
+print_r($_SERVER["uid"]);
+
+?>
+
+<?php
+session_start();
+
+try {
+
+    $query1 = 'select webId from Users where webID = ?';
+    $stmt1 = $conn->prepare($query1);
+    $stmt1->execute([$username]);
+    $row1 = $stmt1->fetch(PDO::FETCH_ASSOC);
+
+    if ($row["webID"] === $username) {
+        if ($row1["roleID"] !== 1) {
+            $_SESSION["user"] = $username;
+            redirect("https://turing.cs.olemiss.edu/~retonos/Rebel-Reviewer/landingpage.php");
+        } else {
+            $_SESSION["admin"] = $username;
+            redirect("https://turing.cs.olemiss.edu/~group1/PHP/admin.php");
+        }
+    } else {
+        redirect("https://turing.cs.olemiss.edu/~retonos/HTML/error.html");
+    }
+
+    Database::dbDisconnect();
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
