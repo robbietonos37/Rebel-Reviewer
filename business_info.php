923c47f9ef085608c5b7c51fd8b0e2a94a8a8178
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-10-09 15:36:24 -0500

    Add styling to posting page and add styling page for it

--- business_info.php
+++ business_info.php
@@ -14,6 +14,8 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -39,8 +41,9 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         while ($row = $statement->fetch()) {
             echo "
-        <div>
+        <div class='d-flex justify-content-center d-col'>
         <h3 class='text-center mb-3'>" . $row['businessName'] . "</h3>
+        <span>Address: " . $row['address'] . "</span>
 
         </div>
 
