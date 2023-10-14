98c65a3658e1b0b41dcd1f969dc28aa54c30697f
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-10-14 17:52:06 -0500

    Add check to see if a webId already exists

--- Authenticate/index.php
+++ Authenticate/index.php
@@ -1,14 +1,47 @@
 <?php
+$webId = json_encode($_SERVER["uid"]);
+session_start();
+require_once("/home/retonos/public_html/connect.php");
+
+$conn = Database::connectDB();
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$query = "SELECT * FROM Users WHERE webId = :webId";
+$statement = $conn->prepare($query);
+$statement->execute(['webId' => $webId]);
+if($statement->rowCount() > 0){
+    echo 'Robbie EXISTS!!' ;
+}
+else {
+    header("Location: https://turing.cs.olemiss.edu/~retonos/Rebel-Reviewer/index.html");
+    exit;
+}
 
 echo '<script>';
-echo 'console.log(' . json_encode($_SERVER) . ')';
+echo 'console.log(' . json_encode($_SERVER) . ' is the authenticated webid)';
 echo '</script>';
 print_r($_SERVER["uid"]);
+print_r($webId);
+
 
 ?>
 
 <?php
 session_start();
+require_once("/home/retonos/public_html/connect.php");
+
+$conn = Database::connectDB();
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$query = "SELECT * FROM users WHERE webId = '$webId'";
+$statement = $conn->execute($query);
+if($statement){
+    echo 'Robbie EXISTS!!';
+}
+else {
+    echo "WE HAVE A PHONEY";
+}
+?>
+
+<?php
 
 try {
 
