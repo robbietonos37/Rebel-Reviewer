25c310a0665872673cc84baf84c34a3ffdefe998
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-10-15 19:06:28 -0500

    Add pages for signed in users

---
+++ Authenticate/signedInCoffeeshops.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+require_once("/home/retonos/public_html/connect.php");
+
+$conn = Database::connectDB();
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <nav class="mt-3">
+        <ul id="left-items">
+            <li><a class="btn fs-5 site-options" href="index.html">Rebel Reviewer</a></li>
+            <li><a class="btn fs-5 site-options" href="howItWorks.html">How does it work?</a></li>
+            <li><a class="btn fs-5 site-options" href="something.html">Contact</a></li>
+        </ul>
+        <ul id="right-items">
+            <li><a class="btn btn-lg business-options" href="restaurants.php">Restaurants</a></li>
+            <li><a class="btn btn-lg business-options" href="bars.php">Bars</a></li>
+        </ul>
+
+    </nav>
+
+    <div id="all-restaurants">
+        <?php
+        try {
+            $query = 'SELECT * FROM businessData AS bd LEFT JOIN businessTypes AS bt ON bt.businessId = bd.businessId WHERE bt.type = "Coffeeshop" ORDER BY businessName';
+            $stmt = $conn->query($query);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $businessId = $row['businessId'];
+
+
+            if($row['url'] !== ''){
+            echo "
+            <div class='mt-3 mb-3 border border-secondary d-flex align-items-center flex-column justify-content-center gap-2 business-posting'>
+            <h3 class='mt-3'> <a href="  . $row['url'] . " target='_blank'>" . $row['businessName'] . "</a></h3>
+            <span>Address: " . $row['address'] . "</span>
+            <span class='text-center'><a href="  . $row['url'] . " target='_blank'>Website</a></span>
+            <span>Overall Rating: " . $row['overallRating'] . "</span>
+            <a href='business_info.php?businessId={$businessId}' class='btn btn-primary view-reviews mb-3'>View Business Info</a>
+        </div>";
+            }
+            else {
+             echo "
+            <div class='mt-3 mb-3 border border-secondary d-flex align-items-center flex-column justify-content-center gap-2 business-posting'>
+            <h3>" . $row['businessName'] . "</h3>
+            <span>Address: " . $row['address'] . "</span>
+            <span>Overall Rating: " . $row['overallRating'] . "</span>
+            <a href='business_info.php?businessId={$businessId}' class='btn btn-primary view-reviews mb-3'>View Business Info</a>
+        </div>";
+            }
+        }
+        ?>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ Authenticate/signedInRestaurants.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+require_once("/home/retonos/public_html/connect.php");
+
+$conn = Database::connectDB();
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <nav class="mt-3">
+        <ul id="left-items">
+            <li><a class="btn fs-5 site-options" href="index.html">Rebel Reviewer</a></li>
+            <li><a class="btn fs-5 site-options" href="howItWorks.html">How does it work?</a></li>
+            <li><a class="btn fs-5 site-options" href="something.html">Contact</a></li>
+        </ul>
+        <ul id="right-items">
+            <li><a class="btn btn-lg business-options" href="coffeeshops.php">Coffeeshops</a></li>
+            <li><a class="btn btn-lg business-options" href="bars.php">Bars</a></li>
+        </ul>
+    </nav>
+
+    <div id="all-restaurants">
+        <?php
+        try {
+            $query = 'SELECT * FROM businessData AS bd LEFT JOIN businessTypes AS bt ON bt.businessId = bd.businessId WHERE bt.type = "Restaurant" ORDER BY businessName';
+            $stmt = $conn->query($query);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+         
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $businessId = $row['businessId'];
+
+            if($row['url'] !== ''){
+            echo "
+            <div class='mt-3 mb-3 border border-secondary d-flex align-items-center flex-column justify-content-center gap-2 business-posting'>
+            <h3 class='mt-3'> <a href="  . $row['url'] . " target='_blank'>" . $row['businessName'] . "</a></h3>
+            <span>" . $row['address'] . "</span>
+            <span class='text-center'><a href="  . $row['url'] . " target='_blank'>Website</a></span>
+            <span>Overall Rating: " . $row['overallRating'] . "</span>
+            <a href='business_info.php?businessId={$businessId}' class='btn btn-primary view-reviews mb-3'>View Business Info</a>
+        </div>";
+            }
+            else {
+                echo "
+                <div class='mt-3 mb-3 border border-secondary d-flex align-items-center flex-column justify-content-center gap-2 business-posting'>
+                <h3 class='mt-3'>" . $row['businessName'] . "</h3>
+                <span>" . $row['address'] . "</span>
+                <label>Overall Rating: " . $row['overallRating'] . "</label>
+                <a href='business_info.php?businessId={$businessId}' class='btn btn-primary view-reviews mb-3'>View Business Info</a>
+            </div>";  
+            }
+        }
+        ?>
+    </div>
+
+</body>
+
+</html>
