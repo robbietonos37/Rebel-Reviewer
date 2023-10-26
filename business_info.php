6442f3593ebcbf213f9db969fd0413741a90b6f4
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-10-26 01:10:43 -0500

    Add query to show reviews for business to bizinfo page

--- business_info.php
+++ business_info.php
@@ -54,6 +54,32 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         ?>
 
     </div>
+    <div id="reviews">
+        <?php
+    try{
+        $query = 'SELECT * FROM reviews WHERE businessId = ? AND approved = 1 ORDER BY date_submitted DESC';
+        $statement = $conn->prepare($query);
+        $statement->bindParam(1,$businessId);
+        $statement->execute();
+    } catch(PDOException $e){
+        echo $e->getMessage();
+    }
+    while ($row = $statement->fetch()) {
+        echo "
+    <div class='d-flex align-items-center flex-column justify-content-center gap-2'>
+    <h3 class='text-center mb-3'>Rating Value " . $row['rating'] . "</h3>
+    <span>User: " . $row['webId'] . "</span>
+    <p>Review: " . $row['reviewText'] . "</p>
+    <p>Date: " . $row['date_submitted'] . "</p>
+
+    </div>
+
+    ";
+    }
+
+        ?>
+
+    </div>
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
