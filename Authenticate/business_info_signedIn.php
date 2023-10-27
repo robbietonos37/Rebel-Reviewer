51654c78f9e8b268364809b86085f3244ad3e435
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-10-26 22:09:51 -0500

    Add query to show reviews for bizinfo page signed in

--- Authenticate/business_info_signedIn.php
+++ Authenticate/business_info_signedIn.php
@@ -77,6 +77,34 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     </div>
 
+    <h2 class='text-center'>Reviews</h2>
+    <div id="reviews" class='d-flex align-items-center justify-content-center'>
+        <?php
+    try{
+        $query = 'SELECT * FROM reviews WHERE businessId = ? AND approved = 1 ORDER BY date_submitted DESC';
+        $statement = $conn->prepare($query);
+        $statement->bindParam(1,$businessId);
+        $statement->execute();
+    } catch(PDOException $e){
+        echo $e->getMessage();
+    }
+    while ($row = $statement->fetch()) {
+        echo "
+    <div id='review' class='d-flex align-items-center flex-column justify-content-center'>
+    <h3 class='text-center mb-3'>Rating Value " . $row['rating'] . "</h3>
+    <span>Date: " . $row['date_submitted'] . "</span>
+    <span>User: " . $row['webId'] . "</span>
+    <p>Review: " . $row['reviewText'] . "</p>
+
+    </div>
+
+    ";
+    }
+
+        ?>
+
+    </div>
+
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
