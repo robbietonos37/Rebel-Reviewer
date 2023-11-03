4a866bb8ac44e6329f48a5a1ca95e346c90a719b
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-11-03 11:23:14 -0500

    Add pages for all the businesses and add link on admin page

--- Authenticate/allBusinesses.php
+++ Authenticate/allBusinesses.php
@@ -66,17 +66,17 @@ if (isset($_POST['approve'])) {
         $statement->bindParam(2, $businessId, PDO::PARAM_INT);
         $result = $statement->execute();
         
-        // if ($result) {
-        //     header("Location: https://turing.cs.olemiss.edu/~retonos/Rebel-Reviewer/signedInRestaurants.php");
-        // } else {
-        //     header("Location: https://turing.cs.olemiss.edu/~retonos/Rebel-Reviewer/index.html");
-        // }
+        if ($result) {
+            header("Location: https://turing.cs.olemiss.edu/~retonos/Rebel-Reviewer/signedInRestaurants.php");
+        } else {
+            header("Location: https://turing.cs.olemiss.edu/~retonos/Rebel-Reviewer/index.html");
+        }
     } catch (PDOException $e) {
         echo $e->getMessage();
         echo "Query 2 failing.";
     }
 }
-if (isset($_POST['deny'])) {
+if (isset($_POST['delete'])) {
     
     $reviewId = $_POST['reviewId'];
 
@@ -85,12 +85,12 @@ if (isset($_POST['deny'])) {
     $statement = $conn->prepare($query);
     $statement->bindParam(1,$reviewId);
     $result = $statement->execute();
-    // if($result){
-    //     header("Location: https://turing.cs.olemiss.edu/~retonos/Rebel-Reviewer/Authenticate/signedInBars.php");
-    // }
-    // else {
-    //     header("Location: https://turing.cs.olemiss.edu/~retonos/Rebel-Reviewer/index.html");
-    // }
+    if($result){
+        header("Location: https://turing.cs.olemiss.edu/~retonos/Rebel-Reviewer/Authenticate/signedInBars.php");
+    }
+    else {
+        header("Location: https://turing.cs.olemiss.edu/~retonos/Rebel-Reviewer/index.html");
+    }
     } catch(PDOException $e){
         echo $e->getMessage();
     }
@@ -102,10 +102,6 @@ if (isset($_POST['deny'])) {
             <li><a class="btn btn-lg business-options" href="signedInRestaurants.php">Restaurants</a></li>
             <li><a class="btn btn-lg business-options" href="signedInBars.php">Bars</a></li>
             <li><a class="btn btn-lg business-options" href="signedInCoffeeshops.php">Coffeeshops</a></li>
-
-            <li><a class="btn btn-lg account-action" href="allBusinesses.php">All Businesses</a></li>
-            <li><a class="btn btn-lg account-action" href="logout.php">Sign Out</a></li>
-
         </ul>
     </nav>
 
@@ -115,20 +111,15 @@ if (isset($_POST['deny'])) {
 
     <table id="unapproved-reviews" class='table justify-content-center align-items-center table-bordered'>
         <tr>
-    <td>WebId</td>
     <td>Business Name</td>
-    <td>Rating</td>
-    <td>Review Text</td>
-    <td>Date Submitted</td>
-    <td>Approve</td>
-    <td>Deny</td>
+    <td>Address</td>
+    <td>Delete</td>
+    <td>Edit</td>
 </tr>
     <?php
+    echo "this is session for webId: " .$webId;
     try {
-            $query = 'SELECT reviews.*, businessData.businessName
-FROM reviews
-JOIN businessData ON reviews.businessId = businessData.businessId WHERE reviews.approved = 0
-ORDER BY reviews.date_submitted';
+            $query = 'SELECT * FROM businessData';
             $stmt = $conn->query($query);
         } catch (PDOException $e) {
             echo $e->getMessage();
@@ -137,25 +128,19 @@ ORDER BY reviews.date_submitted';
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo "
-            <form method='post' action='admin.php'>
+            <form method='post' action='allBusinesses.php'>
             <tr>
-            <td>" . $row['webId'] . "</td>
             <td>" . $row['businessName'] . "</td>
-            <td>" . $row['rating'] . "</td>
-            <td>" . $row['reviewText'] . "</td>
-            <td>" . $row['date_submitted'] . "</td>
-            <input type='hidden' name='reviewId' value=" . $row['reviewId'] . ">
+            <td>" . $row['address'] . "</td>
             <input type='hidden' name='businessId' value=" . $row['businessId'] . ">
-            <td><button name='approve' class='btn btn-sm approve' type='submit'>Approve</button></td>
-            <td><button name='deny' class='btn btn-sm deny' type='submit'>Deny</button></td>
+            <td><button name='deny' class='btn btn-sm deny' type='submit'>Delete</button></td>
+            <td><a href='' class='btn btn-sm'></a></td>
             </form>
         </tr>";
         }
 
     ?>
     </table>
-
-    
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
