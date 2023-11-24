b4ecd8cf95d109882f61559def1e12189994f3bf
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-11-23 23:39:44 -0600

    Add cuisine filter

--- Authenticate/signedInRestaurants.php
+++ Authenticate/signedInRestaurants.php
@@ -67,7 +67,7 @@ if($row['isBlacklisted'] == 1){
                         </div>
                         <div id="myDropdown" class="dropdown-content mt-3">
                             <label>Select a Cuisine</label>
-                            <select name="Cuisine">
+                            <select name="cuisineId">
                                 <option value="None" selected>
                                     None Selected
                                 </option>
@@ -79,12 +79,13 @@ if($row['isBlacklisted'] == 1){
                                     echo $e->getMessage();
                                     }
                                     while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-                                        echo "<option value=" . $row['cuisineDesc'] . ">"
+                                        echo "<option value=" . $row['cuisineId'] . ">"
                                         . $row['cuisineDesc'] . 
                                         "</option>";
                                     }
                             ?>
                             </select>
+                            <button type='submit' class='btn site-options btn-lg m-1' name="cuisine-search">Filter</button>
                         </div>
                     </div>    
                 <form>
@@ -96,13 +97,29 @@ if($row['isBlacklisted'] == 1){
 
     <div id="all-restaurants" class='mb-5'>
         <?php
-        if(!isset($_POST['search'])  Or $_POST['restaurantName'] === ''){
+        if((!isset($_POST['search']) And !isset($_POST['cuisine-search'])) Or ($_POST['restaurantName'] === '' And !isset($_POST['cuisine-search']))){
             try {
                 $query = 'SELECT * FROM businessData AS bd LEFT JOIN businessTypes AS bt ON bt.businessId = bd.businessId WHERE bt.type = "Restaurant" ORDER BY businessName';
                 $stmt = $conn->query($query);
             } catch (PDOException $e) {
                 echo $e->getMessage();
             }
+        }
+        else if(isset($_POST['cuisine-search'])){
+            try{
+                $cuisineId = $_POST['cuisineId'];
+                $query = 'SELECT bd.*
+                FROM businessData AS bd
+                JOIN businessTypes AS bt ON bd.businessId = bt.businessId
+                JOIN businessCuisines AS bc ON bd.businessId = bc.businessId
+                WHERE bt.type = "Restaurant" AND bc.cuisineId = ?';
+                $stmt = $conn->prepare($query);
+                //$stmt->bindParam(1,  "%'" . $restaurantName . "'%");
+                $stmt->execute([$cuisineId]);
+                //$result = $stmt->execute();
+                } catch (PDOException $e) {
+                    echo "Error executing the query: " . $e->getMessage();
+                }
         }
         else {
                 try{
